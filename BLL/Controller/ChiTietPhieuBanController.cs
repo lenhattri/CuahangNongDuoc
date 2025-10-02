fa24a03d8f2f728c0ea4a526c9b682e7daa61277
Huy2004P <phathuy2004h@gmail.com>
2025-10-02 14:32:36 +0700

    Thay đổi app.config

--- BLL/Controller/ChiTietPhieuBanController.cs
+++ BLL/Controller/ChiTietPhieuBanController.cs
@@ -1,95 +1,213 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using CuahangNongduoc.DataLayer;
+//using CuahangNongduoc.BusinessObject;
+//using System.Windows.Forms;
+//using System.Data;
+
+//namespace CuahangNongduoc.Controller
+//{
+
+//    public class ChiTietPhieuBanController
+//    {
+//        ChiTietPhieuBanFactory factory = new ChiTietPhieuBanFactory();
+
+
+
+//        public void HienThiChiTiet(DataGridView dgv, String idPhieuBan)
+//        {
+//            BindingSource bs = new BindingSource();
+//            bs.DataSource = factory.LayChiTietPhieuBan(idPhieuBan);
+//            dgv.DataSource = bs;
+//        }
+//        public DataRow NewRow()
+//        {
+//            return factory.NewRow();
+//        }
+//        public void Add(DataRow row)
+//        {
+//            factory.Add(row);
+//        }
+//        public void Save()
+//        {
+//            factory.Save();
+//        }
+
+
+//        public IList<ChiTietPhieuBan> ChiTietPhieuBan(String idPhieuBan)
+//        {
+//            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
+
+//            DataTable tbl = factory.LayChiTietPhieuBan(idPhieuBan);
+//            foreach (DataRow row in tbl.Rows)
+//            {
+//                MaSanPhamController ctrl = new MaSanPhamController();
+//                ChiTietPhieuBan ct = new ChiTietPhieuBan();
+//                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
+//                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
+//                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
+//                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
+
+//                ds.Add(ct);
+//            }
+//            return ds;
+//        }
+
+//        public IList<ChiTietPhieuBan> ChiTietPhieuBan(DateTime dtNgayBan)
+//        {
+//            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
+
+//            DataTable tbl = factory.LayChiTietPhieuBan(dtNgayBan);
+//            foreach (DataRow row in tbl.Rows)
+//            {
+//                MaSanPhamController ctrl = new MaSanPhamController();
+//                ChiTietPhieuBan ct = new ChiTietPhieuBan();
+//                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
+//                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
+//                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
+//                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
+
+//                ds.Add(ct);
+//            }
+//            return ds;
+//        }
+//        public IList<ChiTietPhieuBan> ChiTietPhieuBan(int thang, int nam)
+//        {
+//            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
+
+//            DataTable tbl = factory.LayChiTietPhieuBan(thang, nam);
+//            foreach (DataRow row in tbl.Rows)
+//            {
+//                MaSanPhamController ctrl = new MaSanPhamController();
+//                ChiTietPhieuBan ct = new ChiTietPhieuBan();
+//                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
+//                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
+//                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
+//                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
+
+//                ds.Add(ct);
+//            }
+//            return ds;
+//        }
+
+//    }
+//}
 using System;
 using System.Collections.Generic;
-using System.Text;
-using CuahangNongduoc.DataLayer;
-using CuahangNongduoc.BusinessObject;
-using System.Windows.Forms;
 using System.Data;
+using System.Windows.Forms;
+using CuahangNongduoc.BusinessObject;
+using CuahangNongduoc.DataLayer;
 
 namespace CuahangNongduoc.Controller
 {
-
     public class ChiTietPhieuBanController
     {
-        ChiTietPhieuBanFactory factory = new ChiTietPhieuBanFactory();
+        // DAL ADO.NET (SqlClient) đã viết ở bước trước
+        private readonly ChiTietPhieuBanDAL _dal = new ChiTietPhieuBanDAL();
 
+        // Bộ đệm hàng Added để Save() một lượt (thay cho DataService cũ)
+        private readonly DataTable _buffer;
 
-
-        public void HienThiChiTiet(DataGridView dgv, String idPhieuBan)
+        public ChiTietPhieuBanController()
         {
-            BindingSource bs = new BindingSource();
-            bs.DataSource = factory.LayChiTietPhieuBan(idPhieuBan);
+            _buffer = CreateBufferSchema();
+        }
+
+        /* ===================== BINDING HIỂN THỊ ===================== */
+        public void HienThiChiTiet(DataGridView dgv, string idPhieuBan)
+        {
+            var bs = new BindingSource
+            {
+                DataSource = _dal.LayChiTietPhieuBan(idPhieuBan)
+            };
             dgv.DataSource = bs;
         }
+
+        /* ===================== API GIỮ NGUYÊN CHO UI ===================== */
         public DataRow NewRow()
         {
-            return factory.NewRow();
+            return _buffer.NewRow();
         }
+
         public void Add(DataRow row)
         {
-            factory.Add(row);
+            // Đảm bảo row thuộc schema _buffer
+            if (row.Table != _buffer)
+            {
+                var newRow = _buffer.NewRow();
+                foreach (DataColumn col in _buffer.Columns)
+                    if (row.Table.Columns.Contains(col.ColumnName))
+                        newRow[col.ColumnName] = row[col.ColumnName];
+                _buffer.Rows.Add(newRow);
+            }
+            else
+            {
+                _buffer.Rows.Add(row);
+            }
         }
+
         public void Save()
         {
-            factory.Save();
+            // Chỉ ghi các row trạng thái Added → DAL sẽ:
+            // 1) Trừ kho
+            // 2) Insert CHI_TIET_PHIEU_BAN (transaction)
+            bool ok = _dal.SaveAddedRows(_buffer);
+
+            if (ok)
+            {
+                _buffer.Clear();        // reset buffer sau khi đã commit
+                _buffer.AcceptChanges();
+            }
         }
 
-
-        public IList<ChiTietPhieuBan> ChiTietPhieuBan(String idPhieuBan)
+        /* ===================== TRẢ VỀ LIST DOMAIN OBJECT ===================== */
+        public IList<ChiTietPhieuBan> ChiTietPhieuBan(string idPhieuBan)
         {
-            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
-
-            DataTable tbl = factory.LayChiTietPhieuBan(idPhieuBan);
-            foreach (DataRow row in tbl.Rows)
-            {
-                MaSanPhamController ctrl = new MaSanPhamController();
-                ChiTietPhieuBan ct = new ChiTietPhieuBan();
-                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
-                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
-                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
-                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
-
-                ds.Add(ct);
-            }
-            return ds;
+            return MapToList(_dal.LayChiTietPhieuBan(idPhieuBan));
         }
 
         public IList<ChiTietPhieuBan> ChiTietPhieuBan(DateTime dtNgayBan)
         {
-            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
-
-            DataTable tbl = factory.LayChiTietPhieuBan(dtNgayBan);
-            foreach (DataRow row in tbl.Rows)
-            {
-                MaSanPhamController ctrl = new MaSanPhamController();
-                ChiTietPhieuBan ct = new ChiTietPhieuBan();
-                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
-                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
-                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
-                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
-
-                ds.Add(ct);
-            }
-            return ds;
+            return MapToList(_dal.LayChiTietPhieuBan(dtNgayBan));
         }
+
         public IList<ChiTietPhieuBan> ChiTietPhieuBan(int thang, int nam)
         {
-            IList<ChiTietPhieuBan> ds = new List<ChiTietPhieuBan>();
+            return MapToList(_dal.LayChiTietPhieuBan(thang, nam));
+        }
 
-            DataTable tbl = factory.LayChiTietPhieuBan(thang, nam);
+        /* ===================== HELPERS ===================== */
+        private static DataTable CreateBufferSchema()
+        {
+            // Tạo schema tối thiểu cần để Insert + cập nhật kho
+            var t = new DataTable("CHI_TIET_PHIEU_BAN");
+            t.Columns.Add("ID_PHIEU_BAN", typeof(string));
+            t.Columns.Add("ID_MA_SAN_PHAM", typeof(string));
+            t.Columns.Add("SO_LUONG", typeof(int));
+            t.Columns.Add("DON_GIA", typeof(decimal));
+            t.Columns.Add("THANH_TIEN", typeof(decimal));
+            return t;
+        }
+
+        private static IList<ChiTietPhieuBan> MapToList(DataTable tbl)
+        {
+            var ds = new List<ChiTietPhieuBan>();
+            var ctrlMaSP = new MaSanPhamController(); // tạo 1 lần, dùng lại
+
             foreach (DataRow row in tbl.Rows)
             {
-                MaSanPhamController ctrl = new MaSanPhamController();
-                ChiTietPhieuBan ct = new ChiTietPhieuBan();
-                ct.DonGia = Convert.ToInt64(row["DON_GIA"]);
-                ct.SoLuong = Convert.ToInt32(row["SO_LUONG"]);
-                ct.ThanhTien = Convert.ToInt64(row["THANH_TIEN"]);
-                ct.MaSanPham = ctrl.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]));
-
+                var ct = new ChiTietPhieuBan
+                {
+                    DonGia = Convert.ToInt64(row["DON_GIA"]),
+                    SoLuong = Convert.ToInt32(row["SO_LUONG"]),
+                    ThanhTien = Convert.ToInt64(row["THANH_TIEN"]),
+                    MaSanPham = ctrlMaSP.LayMaSanPham(Convert.ToString(row["ID_MA_SAN_PHAM"]))
+                };
                 ds.Add(ct);
             }
             return ds;
         }
-
     }
 }
--- DAL/DataLayer/DonViTinhFactory.cs
+++ DAL/DataLayer/DonViTinhFactory.cs
@@ -1,34 +1,130 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Data;
+//using System.Data.OleDb;
+
+//namespace CuahangNongduoc.DataLayer
+//{
+//    public class DonViTinhFactory
+//    {
+//        DataService m_Ds = new DataService();
+
+//        public DataTable DanhsachDVT()
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH");
+//            m_Ds.Load(cmd);
+
+//            return m_Ds;
+//        }
+
+
+//        public DataTable LayDVT(int id)
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH WHERE ID = @id");
+//            cmd.Parameters.Add("id", OleDbType.Integer).Value = id;
+//            m_Ds.Load(cmd);
+//            return m_Ds;
+//        }
+//        public bool Save()
+//        {
+//            return m_Ds.ExecuteNoneQuery() > 0;
+//        }
+//    }
+//}
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Data;
-using System.Data.OleDb;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace CuahangNongduoc.DataLayer
 {
-    public class DonViTinhFactory
+    public class DonViTinhDAL
     {
-        DataService m_Ds = new DataService();
+        private readonly string _cs =
+            ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
 
-        public DataTable DanhsachDVT()
+        // SELECT * FROM DON_VI_TINH
+        public DataTable DanhSachDVT()
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH");
-            m_Ds.Load(cmd);
-
-            return m_Ds;
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand("SELECT * FROM DON_VI_TINH", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                da.Fill(dt);
+            }
+            return dt;
         }
 
-
+        // SELECT * FROM DON_VI_TINH WHERE ID = @id
         public DataTable LayDVT(int id)
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM DON_VI_TINH WHERE ID = @id");
-            cmd.Parameters.Add("id", OleDbType.Integer).Value = id;
-            m_Ds.Load(cmd);
-            return m_Ds;
+            var dt = new DataTable();
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand("SELECT * FROM DON_VI_TINH WHERE ID = @id", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@id", SqlDbType.Int).Value = id;
+                da.Fill(dt);
+            }
+            return dt;
         }
-        public bool Save()
+
+        /* ==== CRUD nhanh (tuỳ dùng) ==== */
+        // Nếu bảng có cột TEN (và có thể có GHI_CHU). Bổ sung cột nếu DB của bạn khác.
+        public int Insert(string ten, string ghiChu = null)
         {
-            return m_Ds.ExecuteNoneQuery() > 0;
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "INSERT INTO DON_VI_TINH(TEN, GHI_CHU) VALUES(@TEN, @GHI_CHU)", conn))
+            {
+                cmd.Parameters.Add("@TEN", SqlDbType.NVarChar, 100).Value = ten;
+                cmd.Parameters.Add("@GHI_CHU", SqlDbType.NVarChar, 255).Value =
+                    (object)ghiChu ?? DBNull.Value;
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Update(int id, string ten, string ghiChu = null)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "UPDATE DON_VI_TINH SET TEN=@TEN, GHI_CHU=@GHI_CHU WHERE ID=@ID", conn))
+            {
+                cmd.Parameters.Add("@TEN", SqlDbType.NVarChar, 100).Value = ten;
+                cmd.Parameters.Add("@GHI_CHU", SqlDbType.NVarChar, 255).Value =
+                    (object)ghiChu ?? DBNull.Value;
+                cmd.Parameters.Add("@ID", SqlDbType.Int).Value = id;
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        public int Delete(int id)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "DELETE FROM DON_VI_TINH WHERE ID=@ID", conn))
+            {
+                cmd.Parameters.Add("@ID", SqlDbType.Int).Value = id;
+                conn.Open();
+                return cmd.ExecuteNonQuery();
+            }
+        }
+
+        /* ==== Phiên bản "Save(DataTable)" tương tự DataService.ExecuteNoneQuery() ==== */
+        // Gọi khi bạn đang có 1 DataTable đã Add/Modify/Delete rows và muốn đẩy lên DB.
+        public bool Save(DataTable table)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var da = new SqlDataAdapter("SELECT * FROM DON_VI_TINH", conn))
+            using (var cb = new SqlCommandBuilder(da))
+            {
+                // SqlCommandBuilder tự sinh Insert/Update/Delete nếu SELECT có cột khóa (ID).
+                return da.Update(table) > 0;
+            }
         }
     }
 }
