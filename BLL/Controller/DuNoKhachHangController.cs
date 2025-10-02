078aa7c4f87c69c0af09d4d9e4d950fdfc1a5e21
Huy2004P <phathuy2004h@gmail.com>
2025-10-02 15:05:19 +0700

    Đã xong dư nợ khách hàng!

--- BLL/Controller/DuNoKhachHangController.cs
+++ BLL/Controller/DuNoKhachHangController.cs
@@ -1,50 +1,119 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Data;
+//using System.Windows.Forms;
+//using CuahangNongduoc.BusinessObject;
+//using CuahangNongduoc.DataLayer;
+
+//namespace CuahangNongduoc.Controller
+//{
+//    public class DuNoKhachHangController
+//    {
+//        DuNoKhachHangFactory factory = new DuNoKhachHangFactory();
+
+//        public void Tonghop(int thang, int nam, 
+//            ToolStripProgressBar bar, DataGridView dg, BindingNavigator bn)
+//        {
+
+//            int ThangTruoc=0, NamTruoc=0;
+//            ThamSo.PreMonth(ref ThangTruoc, ref NamTruoc, thang, nam);
+//            factory.Clear(thang, nam);
+
+
+//            BindingSource bs = new BindingSource();
+//            bs.DataSource = factory.LayDuNoKhachHang("-1", 0, 0);
+//            bn.BindingSource = bs;
+//            dg.DataSource = bs;
+
+//            KhachHangFactory factoryKH = new KhachHangFactory();
+//            DataTable tbl = factoryKH.DanhsachKhachHang();
+//            long dauky = 0, phatsinh = 0, datra = 0, cuoiky = 0;
+
+//            bar.Minimum = 0;
+//            bar.Value = 0;
+//            bar.Maximum = tbl.Rows.Count;
+
+//            foreach(DataRow row in tbl.Rows)
+//            {
+//                DataRow r = factory.NewRow();
+//                String kh = Convert.ToString(row["ID"]);
+
+
+//                dauky = DuNoKhachHangFactory.LayDuNo(kh, ThangTruoc, NamTruoc);
+//                phatsinh = PhieuBanFactory.LayConNo(kh, thang, nam);
+//                datra = PhieuThanhToanFactory.LayTongTien(kh, thang, nam);
+//                cuoiky = (dauky + phatsinh - datra);
+
+//                r["ID_KHACH_HANG"] = kh;
+//                r["THANG"] = thang;
+//                r["NAM"] = nam;
+//                r["DAU_KY"] = dauky;
+//                r["PHAT_SINH"] = phatsinh;
+//                r["DA_TRA"] = datra;
+//                r["CUOI_KY"] = cuoiky;
+
+
+//                factory.Add(r);
+
+//                bar.Value++;
+//            }
+
+
+//        }
+//        public bool Save()
+//        {
+//            return factory.Save();
+//        }
+//    }
+//}
 using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Data;
 using System.Windows.Forms;
-using CuahangNongduoc.BusinessObject;
-using CuahangNongduoc.DataLayer;
+using CuahangNongduoc.DataLayer; // DuNoKhachHangDAL + các Factory cũ
 
 namespace CuahangNongduoc.Controller
 {
     public class DuNoKhachHangController
     {
-        DuNoKhachHangFactory factory = new DuNoKhachHangFactory();
+        private readonly DuNoKhachHangDAL _duNoDal = new DuNoKhachHangDAL();
+        private readonly KhachHangFactory _khFactory = new KhachHangFactory();
 
-        public void Tonghop(int thang, int nam, 
+        public void Tonghop(int thang, int nam,
             ToolStripProgressBar bar, DataGridView dg, BindingNavigator bn)
         {
+            int thangTruoc = 0, namTruoc = 0;
+            ThamSo.PreMonth(ref thangTruoc, ref namTruoc, thang, nam);
 
-            int ThangTruoc=0, NamTruoc=0;
-            ThamSo.PreMonth(ref ThangTruoc, ref NamTruoc, thang, nam);
-            factory.Clear(thang, nam);
+            // 1) Xoá dữ liệu tổng hợp kỳ hiện tại
+            _duNoDal.Clear(thang, nam);
 
+            // 2) Lấy bảng rỗng đúng schema để bind và để DAL giữ _table nội bộ
+            var dtBind = _duNoDal.DanhsachDuNo(thang, nam);
 
-            BindingSource bs = new BindingSource();
-            bs.DataSource = factory.LayDuNoKhachHang("-1", 0, 0);
-            bn.BindingSource = bs;
-            dg.DataSource = bs;
-            
-            KhachHangFactory factoryKH = new KhachHangFactory();
-            DataTable tbl = factoryKH.DanhsachKhachHang();
-            long dauky = 0, phatsinh = 0, datra = 0, cuoiky = 0;
+            var bs = new BindingSource { DataSource = dtBind };
+            if (bn != null) bn.BindingSource = bs;
+            if (dg != null) dg.DataSource = bs;
 
-            bar.Minimum = 0;
-            bar.Value = 0;
-            bar.Maximum = tbl.Rows.Count;
+            var tblKh = _khFactory.DanhsachKhachHang();
 
-            foreach(DataRow row in tbl.Rows)
+            if (bar != null)
             {
-                DataRow r = factory.NewRow();
-                String kh = Convert.ToString(row["ID"]);
-                
+                bar.Minimum = 0;
+                bar.Value = 0;
+                bar.Maximum = tblKh.Rows.Count;
+            }
 
-                dauky = DuNoKhachHangFactory.LayDuNo(kh, ThangTruoc, NamTruoc);
-                phatsinh = PhieuBanFactory.LayConNo(kh, thang, nam);
-                datra = PhieuThanhToanFactory.LayTongTien(kh, thang, nam);
-                cuoiky = (dauky + phatsinh - datra);
+            foreach (DataRow row in tblKh.Rows)
+            {
+                string kh = Convert.ToString(row["ID"]);
 
+                long dauky = DuNoKhachHangDAL.LayDuNo(kh, thangTruoc, namTruoc);
+                long phatsinh = PhieuBanFactory.LayConNo(kh, thang, nam);          // Factory cũ
+                long datra = PhieuThanhToanFactory.LayTongTien(kh, thang, nam); // Factory cũ
+                long cuoiky = dauky + phatsinh - datra;
+
+                DataRow r = _duNoDal.NewRow();
                 r["ID_KHACH_HANG"] = kh;
                 r["THANG"] = thang;
                 r["NAM"] = nam;
@@ -53,17 +122,15 @@ namespace CuahangNongduoc.Controller
                 r["DA_TRA"] = datra;
                 r["CUOI_KY"] = cuoiky;
 
-           
-                factory.Add(r);
+                _duNoDal.Add(r);
 
-                bar.Value++;
+                if (bar != null && bar.Value < bar.Maximum) bar.Value++;
             }
-
-            
         }
+
         public bool Save()
         {
-            return factory.Save();
+            return _duNoDal.Save();
         }
     }
 }
--- DAL/DataLayer/DuNoKhachHangFactory.cs
+++ DAL/DataLayer/DuNoKhachHangFactory.cs
@@ -1,4 +1,4 @@
-//using System;
+﻿//using System;
 //using System.Collections.Generic;
 //using System.Text;
 //using System.Data;
@@ -80,3 +80,147 @@
 //        }
 //    }
 //}
+using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CuahangNongduoc.DataLayer
+{
+    public class DuNoKhachHangDAL
+    {
+        private readonly string _cs =
+            ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+        // DataTable trung tâm, tương tự m_Ds trong bản cũ
+        private DataTable _table;
+
+        private const string SELECT_ALL =
+            "SELECT ID_KHACH_HANG, THANG, NAM, DAU_KY, PHAT_SINH, DA_TRA, CUOI_KY FROM DU_NO_KH";
+
+        /* ========================= Helpers ========================= */
+
+        private SqlDataAdapter CreateAdapter(SqlConnection conn)
+        {
+            var da = new SqlDataAdapter(SELECT_ALL, conn)
+            {
+                // Lấy đủ schema/PK để SqlCommandBuilder sinh lệnh CRUD
+                MissingSchemaAction = MissingSchemaAction.AddWithKey
+            };
+            var cb = new SqlCommandBuilder(da);
+            // (cb sẽ tự gán da.InsertCommand/UpdateCommand/DeleteCommand)
+            return da;
+        }
+
+        private void EnsureSchema()
+        {
+            if (_table != null) return;
+            LoadSchema();
+        }
+
+        /* ========================= API tương đương ========================= */
+
+        public void LoadSchema()
+        {
+            // Tương đương: SELECT * WHERE '-1' để lấy schema rỗng
+            using (var conn = new SqlConnection(_cs))
+            using (var da = new SqlDataAdapter(SELECT_ALL + " WHERE 1=0", conn))
+            {
+                _table = new DataTable("DU_NO_KH");
+                da.Fill(_table); // fill schema (rỗng dữ liệu)
+            }
+        }
+
+        public DataTable DanhsachDuNo(int thang, int nam)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                SELECT_ALL + " WHERE THANG=@thang AND NAM=@nam", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@thang", SqlDbType.Int).Value = thang;
+                cmd.Parameters.Add("@nam", SqlDbType.Int).Value = nam;
+
+                var dt = new DataTable("DU_NO_KH");
+                da.Fill(dt);
+
+                // Đồng bộ _table để NewRow()/Add()/Save() dùng cùng schema
+                _table = dt;
+                return dt;
+            }
+        }
+
+        public DataTable LayDuNoKhachHang(string kh, int thang, int nam)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                SELECT_ALL + " WHERE ID_KHACH_HANG=@kh AND THANG=@thang AND NAM=@nam", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@kh", SqlDbType.VarChar, 50).Value = kh;
+                cmd.Parameters.Add("@thang", SqlDbType.Int).Value = thang;
+                cmd.Parameters.Add("@nam", SqlDbType.Int).Value = nam;
+
+                var dt = new DataTable("DU_NO_KH");
+                da.Fill(dt);
+
+                _table = dt; // giữ schema cho NewRow()/Add()/Save()
+                return dt;
+            }
+        }
+
+        public static long LayDuNo(string kh, int thang, int nam)
+        {
+            var cs = ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+            using (var conn = new SqlConnection(cs))
+            using (var cmd = new SqlCommand(
+                "SELECT CUOI_KY FROM DU_NO_KH WHERE ID_KHACH_HANG=@kh AND THANG=@thang AND NAM=@nam", conn))
+            {
+                cmd.Parameters.Add("@kh", SqlDbType.VarChar, 50).Value = kh;
+                cmd.Parameters.Add("@thang", SqlDbType.Int).Value = thang;
+                cmd.Parameters.Add("@nam", SqlDbType.Int).Value = nam;
+
+                conn.Open();
+                object obj = cmd.ExecuteScalar();
+                return (obj == null || obj == DBNull.Value) ? 0L : Convert.ToInt64(obj);
+            }
+        }
+
+        public void Clear(int thang, int nam)
+        {
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "DELETE FROM DU_NO_KH WHERE THANG=@thang AND NAM=@nam", conn))
+            {
+                cmd.Parameters.Add("@thang", SqlDbType.Int).Value = thang;
+                cmd.Parameters.Add("@nam", SqlDbType.Int).Value = nam;
+
+                conn.Open();
+                cmd.ExecuteNonQuery();
+            }
+        }
+
+        public DataRow NewRow()
+        {
+            EnsureSchema();
+            return _table.NewRow();
+        }
+
+        public void Add(DataRow row)
+        {
+            EnsureSchema();
+            _table.Rows.Add(row);
+        }
+
+        public bool Save()
+        {
+            EnsureSchema();
+            using (var conn = new SqlConnection(_cs))
+            using (var da = CreateAdapter(conn))
+            {
+                return da.Update(_table) > 0;
+            }
+        }
+    }
+}
+
