72617596727c889fdd1de1e635e3cbf016714233
Huy2004P <phathuy2004h@gmail.com>
2025-10-02 16:10:02 +0700

    Thay mới Controller và Factory KhachHang sang ADO.NET

--- BLL/Controller/KhachHangController.cs
+++ BLL/Controller/KhachHangController.cs
@@ -1,103 +1,243 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Data;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Data;
+//using CuahangNongduoc.BusinessObject;
+//using CuahangNongduoc.DataLayer;
+
+
+//namespace CuahangNongduoc.Controller
+//{
+//    public class KhachHangController
+//    {
+//        KhachHangFactory factory = new KhachHangFactory();
+
+//        public void HienthiAutoComboBox(System.Windows.Forms.ComboBox cmb, bool loai)
+//        {
+//            cmb.DataSource = factory.DanhsachKhachHang(loai);
+//            cmb.DisplayMember = "HO_TEN";
+//            cmb.ValueMember = "ID";
+//        }
+//        public void HienthiChungAutoComboBox(System.Windows.Forms.ComboBox cmb)
+//        {
+//            cmb.DataSource = factory.DanhsachKhachHang();
+//            cmb.DisplayMember = "HO_TEN";
+//            cmb.ValueMember = "ID";
+//        }
+
+//        public void HienthiKhachHangDataGridview(System.Windows.Forms.DataGridView dg, System.Windows.Forms.BindingNavigator bn)
+//        {
+//            System.Windows.Forms.BindingSource bs = new System.Windows.Forms.BindingSource();
+//            DataTable tbl = factory.DanhsachKhachHang(false);
+//            tbl.Columns[4].DefaultValue = false;
+//            bs.DataSource = tbl;
+//            bn.BindingSource = bs;
+//            dg.DataSource = bs;
+
+//        }
+
+//        public void HienthiKhachHangChungDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
+//        {
+
+//            cmb.DataSource = factory.DanhsachKhachHang();
+//            cmb.DisplayMember = "HO_TEN";
+//            cmb.ValueMember = "ID";
+//            cmb.DataPropertyName = "ID_KHACH_HANG";
+//            cmb.HeaderText = "Khách hàng";
+
+//        }
+
+//        public void HienthiKhachHangDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
+//        {
+
+//            cmb.DataSource = factory.DanhsachKhachHang(false);
+//            cmb.DisplayMember = "HO_TEN";
+//            cmb.ValueMember = "ID";
+//            cmb.DataPropertyName = "ID_KHACH_HANG";
+//            cmb.HeaderText = "Khách hàng";
+
+//        }
+//        public void HienthiDaiLyDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
+//        {
+
+//            cmb.DataSource = factory.DanhsachKhachHang(true);
+//            cmb.DisplayMember = "HO_TEN";
+//            cmb.ValueMember = "ID";
+//            cmb.DataPropertyName = "ID_KHACH_HANG";
+//            cmb.HeaderText = "Đại lý";
+
+//        }
+//        public void HienthiDaiLyDataGridview(System.Windows.Forms.DataGridView dg, System.Windows.Forms.BindingNavigator bn)
+//        {
+//            System.Windows.Forms.BindingSource bs = new System.Windows.Forms.BindingSource();
+//            DataTable tbl = factory.DanhsachKhachHang(true);
+//            tbl.Columns[4].DefaultValue = true;
+//            bs.DataSource = tbl;
+//            bn.BindingSource = bs;
+//            dg.DataSource = bs;
+
+//        }
+
+//        public void TimHoTen(String hoten, bool loai)
+//        {
+//            factory.TimHoTen(hoten, loai);
+//        }
+//        public void TimDiaChi(String diachi, bool loai)
+//        {
+//            factory.TimDiaChi(diachi, loai);
+//        }
+
+//        public KhachHang LayKhachHang(String id)
+//        {
+//            DataTable tbl = factory.LayKhachHang(id);
+//            KhachHang kh = new KhachHang();
+//            if (tbl.Rows.Count > 0)
+//            {
+//                kh.Id = Convert.ToString(tbl.Rows[0]["ID"]);
+//                kh.HoTen = Convert.ToString(tbl.Rows[0]["HO_TEN"]);
+//                kh.DienThoai = Convert.ToString(tbl.Rows[0]["DIEN_THOAI"]);
+//                kh.DiaChi = Convert.ToString(tbl.Rows[0]["DIA_CHI"]);
+//                kh.LoaiKH = Convert.ToBoolean(tbl.Rows[0]["LOAI_KH"]);
+//            }
+//            return kh;
+//        }
+
+//        public IList<KhachHang> LayDanhSachKhachHang()
+//        {
+//            DataTable tbl = factory.DanhsachKhachHang();
+//            IList<KhachHang> ds = new List<KhachHang>();
+
+//            foreach (DataRow row in tbl.Rows)
+//            {
+//                KhachHang kh = new KhachHang();
+//                kh.Id = Convert.ToString(row["ID"]);
+//                kh.HoTen = Convert.ToString(row["HO_TEN"]);
+//                kh.DienThoai = Convert.ToString(row["DIEN_THOAI"]);
+//                kh.DiaChi = Convert.ToString(row["DIA_CHI"]);
+//                kh.LoaiKH = Convert.ToBoolean(row["LOAI_KH"]);
+//                ds.Add(kh);
+//            }
+//            return ds;
+//        }
+//        public DataRow NewRow()
+//        {
+//            return factory.NewRow();
+//        }
+//        public void Add(DataRow row)
+//        {
+//            factory.Add(row);
+//        }
+//        public bool Save()
+//        {
+//            return factory.Save();
+//        }
+//    }
+//}
+
 using CuahangNongduoc.BusinessObject;
 using CuahangNongduoc.DataLayer;
-
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Windows.Forms;
 
 namespace CuahangNongduoc.Controller
 {
     public class KhachHangController
     {
-        KhachHangFactory factory = new KhachHangFactory();
+        private readonly KhachHangFactory factory = new KhachHangFactory();
 
-        public void HienthiAutoComboBox(System.Windows.Forms.ComboBox cmb, bool loai)
+        public void HienthiAutoComboBox(ComboBox cmb, bool loai)
         {
             cmb.DataSource = factory.DanhsachKhachHang(loai);
             cmb.DisplayMember = "HO_TEN";
             cmb.ValueMember = "ID";
         }
-        public void HienthiChungAutoComboBox(System.Windows.Forms.ComboBox cmb)
+
+        public void HienthiChungAutoComboBox(ComboBox cmb)
         {
             cmb.DataSource = factory.DanhsachKhachHang();
             cmb.DisplayMember = "HO_TEN";
             cmb.ValueMember = "ID";
         }
 
-        public void HienthiKhachHangDataGridview(System.Windows.Forms.DataGridView dg, System.Windows.Forms.BindingNavigator bn)
+        public void HienthiKhachHangDataGridview(DataGridView dg, BindingNavigator bn)
         {
-            System.Windows.Forms.BindingSource bs = new System.Windows.Forms.BindingSource();
+            var bs = new BindingSource();
             DataTable tbl = factory.DanhsachKhachHang(false);
-            tbl.Columns[4].DefaultValue = false;
+
+            // Đặt default theo TÊN cột thay vì index 4 để an toàn khi đổi DB/schema
+            if (tbl.Columns.Contains("LOAI_KH"))
+                tbl.Columns["LOAI_KH"].DefaultValue = false;
+
             bs.DataSource = tbl;
             bn.BindingSource = bs;
             dg.DataSource = bs;
-            
         }
 
-        public void HienthiKhachHangChungDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
+        public void HienthiKhachHangChungDataGridviewComboBox(DataGridViewComboBoxColumn cmb)
         {
-
             cmb.DataSource = factory.DanhsachKhachHang();
             cmb.DisplayMember = "HO_TEN";
             cmb.ValueMember = "ID";
             cmb.DataPropertyName = "ID_KHACH_HANG";
             cmb.HeaderText = "Khách hàng";
-
         }
 
-        public void HienthiKhachHangDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
+        public void HienthiKhachHangDataGridviewComboBox(DataGridViewComboBoxColumn cmb)
         {
-        
             cmb.DataSource = factory.DanhsachKhachHang(false);
             cmb.DisplayMember = "HO_TEN";
             cmb.ValueMember = "ID";
             cmb.DataPropertyName = "ID_KHACH_HANG";
             cmb.HeaderText = "Khách hàng";
-
         }
-        public void HienthiDaiLyDataGridviewComboBox(System.Windows.Forms.DataGridViewComboBoxColumn cmb)
-        {
 
+        public void HienthiDaiLyDataGridviewComboBox(DataGridViewComboBoxColumn cmb)
+        {
             cmb.DataSource = factory.DanhsachKhachHang(true);
             cmb.DisplayMember = "HO_TEN";
             cmb.ValueMember = "ID";
             cmb.DataPropertyName = "ID_KHACH_HANG";
             cmb.HeaderText = "Đại lý";
-
         }
-        public void HienthiDaiLyDataGridview(System.Windows.Forms.DataGridView dg, System.Windows.Forms.BindingNavigator bn)
+
+        public void HienthiDaiLyDataGridview(DataGridView dg, BindingNavigator bn)
         {
-            System.Windows.Forms.BindingSource bs = new System.Windows.Forms.BindingSource();
+            var bs = new BindingSource();
             DataTable tbl = factory.DanhsachKhachHang(true);
-            tbl.Columns[4].DefaultValue = true;
+
+            if (tbl.Columns.Contains("LOAI_KH"))
+                tbl.Columns["LOAI_KH"].DefaultValue = true;
+
             bs.DataSource = tbl;
             bn.BindingSource = bs;
             dg.DataSource = bs;
-
         }
 
-        public void TimHoTen(String hoten, bool loai)
+        public void TimHoTen(string hoten, bool loai)
         {
+            // Nếu form muốn hiện kết quả tìm, hãy bind DataSource = factory.TimHoTen(...)
             factory.TimHoTen(hoten, loai);
         }
-        public void TimDiaChi(String diachi, bool loai)
+
+        public void TimDiaChi(string diachi, bool loai)
         {
             factory.TimDiaChi(diachi, loai);
         }
-        
-        public KhachHang LayKhachHang(String id)
+
+        public KhachHang LayKhachHang(string id)
         {
             DataTable tbl = factory.LayKhachHang(id);
             KhachHang kh = new KhachHang();
             if (tbl.Rows.Count > 0)
             {
-                kh.Id = Convert.ToString(tbl.Rows[0]["ID"]);
-                kh.HoTen = Convert.ToString(tbl.Rows[0]["HO_TEN"]);
-                kh.DienThoai = Convert.ToString(tbl.Rows[0]["DIEN_THOAI"]);
-                kh.DiaChi = Convert.ToString(tbl.Rows[0]["DIA_CHI"]);
-                kh.LoaiKH = Convert.ToBoolean(tbl.Rows[0]["LOAI_KH"]);
+                var r = tbl.Rows[0];
+                kh.Id = Convert.ToString(r["ID"]);
+                kh.HoTen = Convert.ToString(r["HO_TEN"]);
+                kh.DienThoai = Convert.ToString(r["DIEN_THOAI"]);
+                kh.DiaChi = Convert.ToString(r["DIA_CHI"]);
+                kh.LoaiKH = Convert.ToBoolean(r["LOAI_KH"]);
             }
             return kh;
         }
@@ -106,30 +246,22 @@ namespace CuahangNongduoc.Controller
         {
             DataTable tbl = factory.DanhsachKhachHang();
             IList<KhachHang> ds = new List<KhachHang>();
-
             foreach (DataRow row in tbl.Rows)
             {
-                KhachHang kh = new KhachHang();
-                kh.Id = Convert.ToString(row["ID"]);
-                kh.HoTen = Convert.ToString(row["HO_TEN"]);
-                kh.DienThoai = Convert.ToString(row["DIEN_THOAI"]);
-                kh.DiaChi = Convert.ToString(row["DIA_CHI"]);
-                kh.LoaiKH = Convert.ToBoolean(row["LOAI_KH"]);
-                ds.Add(kh);
+                ds.Add(new KhachHang
+                {
+                    Id = Convert.ToString(row["ID"]),
+                    HoTen = Convert.ToString(row["HO_TEN"]),
+                    DienThoai = Convert.ToString(row["DIEN_THOAI"]),
+                    DiaChi = Convert.ToString(row["DIA_CHI"]),
+                    LoaiKH = Convert.ToBoolean(row["LOAI_KH"])
+                });
             }
             return ds;
         }
-        public DataRow NewRow()
-        {
-            return factory.NewRow();
-        }
-        public void Add(DataRow row)
-        {
-            factory.Add(row);
-        }
-        public bool Save()
-        {
-            return factory.Save();
-        }
+
+        public DataRow NewRow() => factory.NewRow();
+        public void Add(DataRow row) => factory.Add(row);
+        public bool Save() => factory.Save();
     }
 }
--- DAL/DataLayer/KhachHangFactory.cs
+++ DAL/DataLayer/KhachHangFactory.cs
@@ -1,67 +1,201 @@
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using System.Data;
+//using System.Data.OleDb;
+
+//namespace CuahangNongduoc.DataLayer
+//{
+//    public class KhachHangFactory
+//    {
+//        DataService m_Ds = new DataService();
+
+//        public DataTable DanhsachKhachHang(bool loai)
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE LOAI_KH = " + loai);
+//            m_Ds.Load(cmd);
+
+//            return m_Ds;
+//        }
+//        public DataTable TimHoTen(String hoten, bool loai)
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE HO_TEN LIKE '%' + @hoten + '%' AND LOAI_KH = " + loai);
+//            cmd.Parameters.Add("hoten", OleDbType.VarChar).Value = hoten;
+//            m_Ds.Load(cmd);
+
+//            return m_Ds;
+//        }
+
+//        public DataTable TimDiaChi(String diachi, bool loai)
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE DIA_CHI LIKE '%' + @diachi + '%' AND LOAI_KH = " + loai);
+//            cmd.Parameters.Add("diachi", OleDbType.VarChar).Value = diachi;
+//            m_Ds.Load(cmd);
+
+//            return m_Ds;
+//        }
+
+//        public DataTable DanhsachKhachHang()
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG");
+//            m_Ds.Load(cmd);
+
+//            return m_Ds;
+//        }
+
+//        public DataTable LayKhachHang(String id)
+//        {
+//            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE ID = @id");
+//            cmd.Parameters.Add("id", OleDbType.VarChar,50).Value = id;
+//            m_Ds.Load(cmd);
+//            return m_Ds;
+//        }
+
+//        public DataRow NewRow()
+//        {
+//            return m_Ds.NewRow();
+//        }
+//        public void Add(DataRow row)
+//        {
+//            m_Ds.Rows.Add(row);
+//        }
+//        public bool Save()
+//        {
+//            return m_Ds.ExecuteNoneQuery() > 0;
+//        }
+//    }
+//}
 using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Configuration;
 using System.Data;
-using System.Data.OleDb;
+using System.Data.SqlClient;
 
 namespace CuahangNongduoc.DataLayer
 {
     public class KhachHangFactory
     {
-        DataService m_Ds = new DataService();
+        private readonly string _cs =
+            ConfigurationManager.ConnectionStrings["ConnStr"].ConnectionString;
+
+        private DataTable _table;                    // thay cho m_Ds cũ
+        private const string SELECT_ALL = "SELECT * FROM KHACH_HANG";
+
+        /* ===== Helpers ===== */
+        private SqlDataAdapter CreateAdapter(SqlConnection conn)
+        {
+            var da = new SqlDataAdapter(SELECT_ALL, conn)
+            {
+                MissingSchemaAction = MissingSchemaAction.AddWithKey // để SqlCommandBuilder sinh CRUD
+            };
+            var _ = new SqlCommandBuilder(da);
+            return da;
+        }
+
+        private void EnsureSchema()
+        {
+            if (_table != null) return;
+            using (var conn = new SqlConnection(_cs))
+            using (var da = new SqlDataAdapter(SELECT_ALL + " WHERE 1=0", conn))
+            {
+                _table = new DataTable("KHACH_HANG");
+                da.Fill(_table); // chỉ lấy schema
+            }
+        }
+
+        /* ===== API ===== */
 
         public DataTable DanhsachKhachHang(bool loai)
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE LOAI_KH = " + loai);
-            m_Ds.Load(cmd);
-
-            return m_Ds;
-        }
-        public DataTable TimHoTen(String hoten, bool loai)
-        {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE HO_TEN LIKE '%' + @hoten + '%' AND LOAI_KH = " + loai);
-            cmd.Parameters.Add("hoten", OleDbType.VarChar).Value = hoten;
-            m_Ds.Load(cmd);
-
-            return m_Ds;
+            var dt = new DataTable("KHACH_HANG");
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand("SELECT * FROM KHACH_HANG WHERE LOAI_KH = @loai", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@loai", SqlDbType.Bit).Value = loai;
+                da.Fill(dt);
+            }
+            _table = dt;
+            return dt;
         }
 
-        public DataTable TimDiaChi(String diachi, bool loai)
+        public DataTable TimHoTen(string hoten, bool loai)
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE DIA_CHI LIKE '%' + @diachi + '%' AND LOAI_KH = " + loai);
-            cmd.Parameters.Add("diachi", OleDbType.VarChar).Value = diachi;
-            m_Ds.Load(cmd);
+            var dt = new DataTable("KHACH_HANG");
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "SELECT * FROM KHACH_HANG WHERE HO_TEN LIKE '%' + @hoten + '%' AND LOAI_KH = @loai", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@hoten", SqlDbType.NVarChar, 200).Value = hoten ?? "";
+                cmd.Parameters.Add("@loai", SqlDbType.Bit).Value = loai;
+                da.Fill(dt);
+            }
+            _table = dt;
+            return dt;
+        }
 
-            return m_Ds;
+        public DataTable TimDiaChi(string diachi, bool loai)
+        {
+            var dt = new DataTable("KHACH_HANG");
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand(
+                "SELECT * FROM KHACH_HANG WHERE DIA_CHI LIKE '%' + @diachi + '%' AND LOAI_KH = @loai", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@diachi", SqlDbType.NVarChar, 200).Value = diachi ?? "";
+                cmd.Parameters.Add("@loai", SqlDbType.Bit).Value = loai;
+                da.Fill(dt);
+            }
+            _table = dt;
+            return dt;
         }
 
         public DataTable DanhsachKhachHang()
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG");
-            m_Ds.Load(cmd);
-
-            return m_Ds;
+            var dt = new DataTable("KHACH_HANG");
+            using (var conn = new SqlConnection(_cs))
+            using (var da = new SqlDataAdapter(SELECT_ALL, conn))
+            {
+                da.Fill(dt);
+            }
+            _table = dt;
+            return dt;
         }
 
-        public DataTable LayKhachHang(String id)
+        public DataTable LayKhachHang(string id)
         {
-            OleDbCommand cmd = new OleDbCommand("SELECT * FROM KHACH_HANG WHERE ID = @id");
-            cmd.Parameters.Add("id", OleDbType.VarChar,50).Value = id;
-            m_Ds.Load(cmd);
-            return m_Ds;
+            var dt = new DataTable("KHACH_HANG");
+            using (var conn = new SqlConnection(_cs))
+            using (var cmd = new SqlCommand("SELECT * FROM KHACH_HANG WHERE ID = @id", conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                cmd.Parameters.Add("@id", SqlDbType.VarChar, 50).Value = id;
+                da.Fill(dt);
+            }
+            _table = dt;
+            return dt;
         }
 
         public DataRow NewRow()
         {
-            return m_Ds.NewRow();
+            EnsureSchema();
+            return _table.NewRow();
         }
+
         public void Add(DataRow row)
         {
-            m_Ds.Rows.Add(row);
+            EnsureSchema();
+            _table.Rows.Add(row);
         }
+
         public bool Save()
         {
-            return m_Ds.ExecuteNoneQuery() > 0;
+            EnsureSchema();
+            using (var conn = new SqlConnection(_cs))
+            using (var da = CreateAdapter(conn))
+            {
+                return da.Update(_table) > 0;
+            }
         }
     }
 }
