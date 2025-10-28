bbfc4fd7f4f5f5b0287693aac3d7ce4c78992d39
doum4n <pensonic1986@gmail.com>
2025-10-28 22:17:55 +0700

    recommit "sửa lại phần lớn lỗi liên quan đến in"

---
+++ DTO/PhieuChiReport.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuahangNongduoc.DTO
+{
+    public class PhieuChiReport
+    {
+        public string Id { get; set; }
+        public DateTime NgayChi { get; set; }
+        public long TongTien { get; set; }
+        public string LyDoChi { get; set; }
+        public string GhiChu { get; set; }
+    }
+}
--- UI/In/frmInPhieuNhap.cs
+++ UI/In/frmInPhieuNhap.cs
@@ -1,3 +1,4 @@
+﻿using CuahangNongduoc.DTO;
 using CuahangNongduoc.Utils.Functions;
 using System;
 using System.Collections.Generic;
@@ -23,7 +24,24 @@ namespace CuahangNongduoc
         void LocalReport_SubreportProcessing(object sender, Microsoft.Reporting.WinForms.SubreportProcessingEventArgs e)
         {
             e.DataSources.Clear();
-            e.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("ChiTietPhieuNhap", m_PhieuNhap.ChiTiet));
+
+            IList<ChiTietPhieuNhapReport> chiTietPhieuNhapReports = new List<ChiTietPhieuNhapReport>();
+            foreach (var item in m_PhieuNhap.ChiTiet)
+            {
+                ChiTietPhieuNhapReport ct = new ChiTietPhieuNhapReport
+                {
+                    Id = item.Id,
+                    TenSanPham = item.SanPham.TenSanPham,
+                    GiaNhap = item.GiaNhap,
+                    SoLuong = item.SoLuong,
+                    ThanhTien = item.ThanhTien,
+                    NgaySanXuat = item.NgaySanXuat,
+                    NgayHetHan = item.NgayHetHan
+                };
+                chiTietPhieuNhapReports.Add(ct);
+            }
+
+            e.DataSources.Add(new Microsoft.Reporting.WinForms.ReportDataSource("ChiTietPhieuNhap", chiTietPhieuNhapReports));
         }
 
         private void frmInPhieuNhap_Load(object sender, EventArgs e)
@@ -34,12 +52,20 @@ namespace CuahangNongduoc
             param.Add(new Microsoft.Reporting.WinForms.ReportParameter("ten_cua_hang", ch.TenCuaHang));
             param.Add(new Microsoft.Reporting.WinForms.ReportParameter("dia_chi", ch.DiaChi));
             param.Add(new Microsoft.Reporting.WinForms.ReportParameter("dien_thoai", ch.DienThoai));
-
             param.Add(new Microsoft.Reporting.WinForms.ReportParameter("bang_chu", num.NumberToString(m_PhieuNhap.TongTien.ToString())));
+
+            //TODO: fix dữ liệu ngày nhập, tổng tiền không hiển thị
+            PhieuNhapReport phieuNhapReport = new PhieuNhapReport
+            {
+                Id = m_PhieuNhap.Id,
+                NgayNhap = m_PhieuNhap.NgayNhap, // Chưa hiện dữ liệu
+                TongTien = m_PhieuNhap.TongTien, // Chưa hiện dữ liệu
+                TenNhaCungCap = m_PhieuNhap.NhaCungCap?.HoTen,
+            };
 
             ReportHanler.LoadReport(
                 this.reportViewer,
-                new List<CuahangNongduoc.BusinessObject.PhieuNhap>() { m_PhieuNhap },
+                new List<CuahangNongduoc.DTO.PhieuNhapReport>() { phieuNhapReport },
                 "rptPhieuNhap.rdlc",
                 "PhieuNhap", 
                 param
