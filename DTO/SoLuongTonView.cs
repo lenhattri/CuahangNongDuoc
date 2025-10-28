a9076e0526297046f33129bba9dfcf10d5df21ab
lenhattri <lenhattri7@gmail.com>
2025-10-28 16:27:21 +0700

    Fix SoLuongTon RDLC

---
+++ DTO/SoLuongTonView.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CuahangNongduoc.DTO
+{
+    public class SoLuongTonView
+    {
+        public string MaSP { get; set; }          // string cho chắc
+        public string TenSanPham { get; set; }
+        public long DonGiaNhap { get; set; }      // hoặc decimal
+        public long GiaBanLe { get; set; }
+        public long GiaBanSi { get; set; }
+        public string DonViTinh { get; set; }
+        public int SoLuong { get; set; }
+    }
+}
